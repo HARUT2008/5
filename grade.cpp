b29667ee93abab5aeb32f6b952725e29fc14afc3
Harut <HARUTHARUTYUNYAN039@GMAIL.COM>
2025-08-11 12:37:59 +0400

    Update grade.cpp

--- grade.cpp
+++ grade.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
-using namespace std;
+
+char getLetterGrade(int grade) {
+    if (grade >= 90) return 'A';
+    else if (grade >= 80) return 'B';
+    else if (grade >= 70) return 'C';
+    else if (grade >= 60) return 'D';
+    else return 'F';
+}
+
+
+void printGradeArt(char letter) {
+    std::cout << "Your grade is " << letter << "\n";
+
+    switch (letter) {
+        case 'A':
+            std::cout << "[A Excellent!]\n";
+            break;
+        case 'B':
+            std::cout << "[B Good job!]\n";
+            break;
+        case 'C':
+            std::cout << "[C Not bad. Try harder.]\n";
+            break;
+        case 'D':
+            std::cout << "[D Barely passed.]\n";
+            break;
+        case 'F':
+            std::cout << "[F Failed. Study more.]\n";
+            break;
+    }
+}
 
 int main() {
     int grade;
     while (true) {
-        cout << "Enter grade (0-100) or -1 to exit: ";
-        cin >> grade;
-        if (grade == -1)
-            break;
-        if (grade >= 90) cout << "Your grade is A\n";
-        else if (grade >= 80) cout << "Your grade is B\n";
-        else if (grade >= 70) cout << "Your grade is C\n";
-        else if (grade >= 60) cout << "Your grade is D\n";
-        else cout << "Your grade is F\n";
+        std::cout << "Enter grade (0-100) or -1 to exit: ";
+        std::cin >> grade;
+
+        if (grade == -1) break;
+        if (grade < 0 || grade > 100) {
+            std::cout << "Invalid grade! Try again.\n";
+            continue;
+        }
+
+        char letter = getLetterGrade(grade);
+        printGradeArt(letter);
     }
+
     return 0;
 }
